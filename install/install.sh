284631bc704aea406e8949bc18ff26b1118609e4
David Millman <dave@cs.unc.edu>
2024-10-10 11:29:39 -0400

    [BKY-5738] Add installer script (#4)
    * Add installer script
    
    * Update install/install.sh
    
    Co-authored-by: Mike Wittie <mwittie@gmail.com>
    
    * Update call to action
    
    Co-authored-by: Mike Wittie <mwittie@gmail.com>
    
    * Shorten call to action
    
    ---------
    
    Co-authored-by: Mike Wittie <mwittie@gmail.com>

---
+++ install/install.sh
@@ -0,0 +1,117 @@
+#!/usr/bin/env bash
+
+# Exit on error
+set -e
+
+REPO="attestation-service-demo"
+APP="bky-as"
+VERSION="v0.1.0-beta.1"
+
+# let the user know a step was successful
+function passCheck() {
+    echo "✅ $1"
+}
+
+# let the user know that we failed with an error
+function exitWithErr() {
+    echo "❌ ==> $1
+       Could not continue.
+       For feature requests or support please email info@blocky.rocks." >&2
+    exit 1
+}
+
+function getOS() {
+    case "$OSTYPE" in
+        linux*)   echo "linux" ;;
+        darwin*)  echo "darwin" ;;
+        *)        exitWithErr "Unsupported OS" ;;
+    esac
+}
+
+function getArch() {
+    case "$(uname -m)" in
+        x86_64)   echo "amd64" ;;
+        arm64)    echo "arm64" ;;
+        *)        exitWithErr "Unsupported architecture" ;;
+    esac
+}
+
+# check that the os arch combo that the person is installing is supported
+function verifySupport() {
+    local os=$1
+    local arch=$2
+
+    local supported=(linux-amd64 linux-arm64 darwin-amd64 darwin-arm64)
+    local current="$os-$arch"
+
+    for i in "${supported[@]}"; do
+        if [ "$i" == "$current" ]; then
+            passCheck "Your platform is supported: $current"
+            return 0
+        fi
+    done
+
+    printf -v msg \
+        'Your platform (%s) is unsupported. Supported platforms are:\n%s' \
+        "${current}" \
+        "$(printf '       - %s\n' ${supported[@]})"
+    exitWithErr "$msg"
+}
+
+function verifyCurl() {
+    if command -v "curl" > /dev/null; then
+        passCheck "You have curl installed: $(which curl)"
+    else
+        exitWithErr "You do not have curl installed."
+    fi
+}
+
+function downloadCLI() {
+    local os=$1
+    local arch=$2
+
+    local base="https://github.com/blocky/${REPO}/releases/download"
+    local artifact="${APP}_${os}_${arch}"
+    local url="${base}/${VERSION}/${artifact}"
+
+    curl -SsL "${url}" -o "${APP}"
+    chmod +x "${APP}"
+}
+
+function downloadConfig() {
+    local base="https://github.com/blocky/${REPO}/releases/download"
+    local artifact="config.toml"
+    local url="${base}/${VERSION}/${artifact}"
+
+    curl -SsL "${url}" -o "${artifact}"
+}
+
+function verifyCLI() {
+    if command -v "./${APP}" > /dev/null; then
+        passCheck "SUCCESS! You have downloaded the ${APP} CLI"
+    else
+        exitWithErr "download failed"
+    fi
+}
+
+function nextSteps() {
+    cat << EOF
+    To get started, check out the getting started guide and documentation at 
+    https://github.com/blocky/${REPO}
+EOF
+}
+
+
+function main() {
+    local os=$(getOS)
+    local arch=$(getArch)
+
+    verifySupport "$os" "$arch"
+    verifyCurl
+    downloadCLI "$os" "$arch"
+    downloadConfig
+    verifyCLI
+    nextSteps
+}
+
+main
